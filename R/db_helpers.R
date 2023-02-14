b08290b94795f57b3287199885c53b0c11daffe1
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2023-02-14 12:59:20 -0500

    functions to lookup plate and well primary keys

--- R/db_helpers.R
+++ R/db_helpers.R
@@ -32,3 +32,73 @@ existing_barcodes <- function(source_only = FALSE, .db_loc = "./htCE.duckdb",
   )[[1]] |>
     purrr::set_names()
 }
+
+
+
+##----------------------------------------
+##  Get 'plate_id'                      --
+##----------------------------------------
+
+#' Retrieve a plate_id from a barcode string
+#'
+#' @param barcode A string - the unique barcode identifying the physical plate
+#' @param ...
+#' @param .db_loc A string - local path to the DuckDB file; passed to [dkdb_collect()]
+#' @param .db_con A valid DBIConnection object; passed to [dkdb_collect()]
+#' @param .pg_load A Boolean - use the DuckDB Postgres extension?; passed to [dkdb_collect()]
+#'
+#' @return A double value representing the `plate_id` integer primary key which corresponds to the barcode argument
+#' @export
+#'
+barcode_to_plate_id <- function(barcode, ..., .db_loc = "./htCE.duckdb",
+                                .db_con = NULL, .pg_load = FALSE) {
+  dkdb_collect(
+    "SELECT id
+    FROM plates
+    WHERE barcode = {barcode};",
+    barcode = barcode,
+    ...,
+    .db_loc = .db_loc,
+    .db_con = .db_con,
+    .pg_load = .pg_load
+  )[[1]]# |>
+  # bit64::as.integer64()
+}
+
+
+
+##-----------------------------------------
+##  Get ebase 'well_id'                  --
+##-----------------------------------------
+
+#' Retrieve a well_id from barcode and address strings
+#'
+#' @param barcode A string - the unique barcode identifying the physical plate
+#' @param address A string - the well identifier (e.g. A1, H12, etc.)
+#' @param .db_loc A string - local path to the DuckDB file; passed to [dkdb_collect()]
+#' @param .db_con A valid DBIConnection object; passed to [dkdb_collect()]
+#' @param .pg_load A Boolean - use the DuckDB Postgres extension?; passed to [dkdb_collect()]
+#'
+#' @return A double value representing the `well_id` integer primary key which corresponds to the barcode and address arguments
+#' @export
+#'
+address_to_well_id <- function(barcode, address, .db_loc = "./htCE.duckdb",
+                               .db_con = NULL, .pg_load = FALSE) {
+  dkdb_collect(
+    "SELECT w.id
+    FROM wells w
+    WHERE EXISTS (
+      SELECT *
+      FROM plates p
+      WHERE p.barcode = {barcode}
+        AND p.id = w.plate_id
+    )
+      AND w.address = {address};",
+    barcode = barcode,
+    address = address,
+    .db_loc = .db_loc,
+    .db_con = .db_con,
+    .pg_load = .pg_load
+  )[[1]]# |>
+  # bit64::as.integer64()
+}
